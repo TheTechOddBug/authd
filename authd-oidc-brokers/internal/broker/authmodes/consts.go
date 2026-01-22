3v1n0/authd-oidc-brokers

broker: Expose supported authentication modes in const variables
We're using them everywhere in the various providers implementations, so
prevent potential issues due to mistyping variables or anything like
that, making also code easier to maintain and refactor if something is
going to change.

internal/broker/authmodes/consts.go
@@ -1,0 +1,13 @@
+// Package authmodes lists the authentication modes that providers can support.
+package authmodes
+
+const (
+	// PasswordID is the ID of the password authentication method.
+	PasswordID = "password"
+
+	// DeviceID is the ID of the device authentication method.
+	DeviceID = "device_auth"
+
+	// NewPasswordID is the ID of the new password configuration method.
+	NewPasswordID = "newpassword"
+)

internal/broker/broker.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/google/uuid"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers"
 	providerErrors "github.com/ubuntu/authd-oidc-brokers/internal/providers/errors"
@@ -218,7 +219,7 @@ func (b *Broker) GetAuthenticationModes(sessionID string, supportedUILayouts []m
 
 	endpoints := make(map[string]struct{})
 	if session.authCfg.provider != nil && session.authCfg.provider.Endpoint().DeviceAuthURL != "" {
-		endpoints["device_auth"] = struct{}{}
+		endpoints[authmodes.DeviceID] = struct{}{}
 	}
 
 	availableModes, err := b.providerInfo.CurrentAuthenticationModesOffered(
@@ -260,16 +261,16 @@ func (b *Broker) supportedAuthModesFromLayout(supportedUILayouts []map[string]st
 			if !strings.Contains(layout["wait"], "true") {
 				continue
 			}
-			supportedModes["device_auth"] = "Device Authentication"
+			supportedModes[authmodes.DeviceID] = "Device Authentication"
 
 		case "form":
 			if slices.Contains(supportedEntries, "chars_password") {
-				supportedModes["password"] = "Local Password Authentication"
+				supportedModes[authmodes.PasswordID] = "Local Password Authentication"
 			}
 
 		case "newpassword":
 			if slices.Contains(supportedEntries, "chars_password") {
-				supportedModes["newpassword"] = "Define your local password"
+				supportedModes[authmodes.NewPasswordID] = "Define your local password"
 			}
 		}
 	}
@@ -311,7 +312,7 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 
 	var uiLayout map[string]string
 	switch authModeID {
-	case "device_auth":
+	case authmodes.DeviceID:
 		ctx, cancel := context.WithTimeout(context.Background(), maxRequestDuration)
 		defer cancel()
 		response, err := session.authCfg.oauth.DeviceAuth(ctx)
@@ -332,14 +333,14 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 			"code":    response.UserCode,
 		}
 
-	case "password":
+	case authmodes.PasswordID:
 		uiLayout = map[string]string{
 			"type":  "form",
 			"label": "Enter your local password",
 			"entry": "chars_password",
 		}
 
-	case "newpassword":
+	case authmodes.NewPasswordID:
 		label := "Create a local password"
 		if session.mode == "passwd" {
 			label = "Update your local password"
@@ -433,7 +434,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 
 	var authInfo authCachedInfo
 	switch session.selectedMode {
-	case "device_auth":
+	case authmodes.DeviceID:
 		response, ok := session.authInfo["response"].(*oauth2.DeviceAuthResponse)
 		if !ok {
 			slog.Error("could not get device auth response")
@@ -471,7 +472,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 		session.authInfo["auth_info"] = authInfo
 		return AuthNext, nil
 
-	case "password":
+	case authmodes.PasswordID:
 		authInfo, err = b.loadAuthInfo(ctx, session, challenge)
 		if err != nil {
 			slog.Error(err.Error())
@@ -496,7 +497,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 			return AuthNext, nil
 		}
 
-	case "newpassword":
+	case authmodes.NewPasswordID:
 		if challenge == "" {
 			return AuthRetry, errorMessage{Message: "empty challenge"}
 		}

internal/providers/msentraid/msentraid.go
@@ -17,6 +17,7 @@ import (
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
 	msgraphauth "github.com/microsoftgraph/msgraph-sdk-go-core/authentication"
 	msgraphmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	providerErrors "github.com/ubuntu/authd-oidc-brokers/internal/providers/errors"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
@@ -230,20 +231,20 @@ func (p Provider) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.PasswordID}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceID}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.PasswordID}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 	}
 

internal/providers/noprovider/noprovider.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
 	"golang.org/x/oauth2"
 )
@@ -50,20 +51,20 @@ func (p NoProvider) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.PasswordID}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceID}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.PasswordID}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 	}
 

internal/testutils/provider.go
@@ -20,6 +20,7 @@ import (
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/go-jose/go-jose/v4"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
 	"golang.org/x/oauth2"
@@ -415,20 +416,20 @@ func (p MockProviderInfoer) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.PasswordID}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.DeviceID]; ok && providerReachable {
+			offeredModes = []string{authmodes.DeviceID}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.PasswordID}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPasswordID}
 		}
 	}
 

TheTechOddBug/authd

authd-oidc-brokers/internal/broker/authmodes/consts.go
@@ -1,0 +1,13 @@
+// Package authmodes lists the authentication modes that providers can support.
+package authmodes
+
+const (
+	// Password is the ID of the password authentication method.
+	Password = "password"
+
+	// Device is the ID of the device authentication method.
+	Device = "device_auth"
+
+	// NewPassword is the ID of the new password configuration method.
+	NewPassword = "newpassword"
+)

authd-oidc-brokers/internal/broker/broker.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/google/uuid"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers"
 	providerErrors "github.com/ubuntu/authd-oidc-brokers/internal/providers/errors"
@@ -218,7 +219,7 @@ func (b *Broker) GetAuthenticationModes(sessionID string, supportedUILayouts []m
 
 	endpoints := make(map[string]struct{})
 	if session.authCfg.provider != nil && session.authCfg.provider.Endpoint().DeviceAuthURL != "" {
-		endpoints["device_auth"] = struct{}{}
+		endpoints[authmodes.Device] = struct{}{}
 	}
 
 	availableModes, err := b.providerInfo.CurrentAuthenticationModesOffered(
@@ -260,16 +261,16 @@ func (b *Broker) supportedAuthModesFromLayout(supportedUILayouts []map[string]st
 			if !strings.Contains(layout["wait"], "true") {
 				continue
 			}
-			supportedModes["device_auth"] = "Device Authentication"
+			supportedModes[authmodes.Device] = "Device Authentication"
 
 		case "form":
 			if slices.Contains(supportedEntries, "chars_password") {
-				supportedModes["password"] = "Local Password Authentication"
+				supportedModes[authmodes.Password] = "Local Password Authentication"
 			}
 
 		case "newpassword":
 			if slices.Contains(supportedEntries, "chars_password") {
-				supportedModes["newpassword"] = "Define your local password"
+				supportedModes[authmodes.NewPassword] = "Define your local password"
 			}
 		}
 	}
@@ -311,7 +312,7 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 
 	var uiLayout map[string]string
 	switch authModeID {
-	case "device_auth":
+	case authmodes.Device:
 		ctx, cancel := context.WithTimeout(context.Background(), maxRequestDuration)
 		defer cancel()
 		response, err := session.authCfg.oauth.DeviceAuth(ctx)
@@ -332,14 +333,14 @@ func (b *Broker) generateUILayout(session *sessionInfo, authModeID string) (map[
 			"code":    response.UserCode,
 		}
 
-	case "password":
+	case authmodes.Password:
 		uiLayout = map[string]string{
 			"type":  "form",
 			"label": "Enter your local password",
 			"entry": "chars_password",
 		}
 
-	case "newpassword":
+	case authmodes.NewPassword:
 		label := "Create a local password"
 		if session.mode == "passwd" {
 			label = "Update your local password"
@@ -433,7 +434,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 
 	var authInfo authCachedInfo
 	switch session.selectedMode {
-	case "device_auth":
+	case authmodes.Device:
 		response, ok := session.authInfo["response"].(*oauth2.DeviceAuthResponse)
 		if !ok {
 			slog.Error("could not get device auth response")
@@ -471,7 +472,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 		session.authInfo["auth_info"] = authInfo
 		return AuthNext, nil
 
-	case "password":
+	case authmodes.Password:
 		authInfo, err = b.loadAuthInfo(ctx, session, challenge)
 		if err != nil {
 			slog.Error(err.Error())
@@ -496,7 +497,7 @@ func (b *Broker) handleIsAuthenticated(ctx context.Context, session *sessionInfo
 			return AuthNext, nil
 		}
 
-	case "newpassword":
+	case authmodes.NewPassword:
 		if challenge == "" {
 			return AuthRetry, errorMessage{Message: "empty challenge"}
 		}

authd-oidc-brokers/internal/providers/msentraid/msentraid.go
@@ -17,6 +17,7 @@ import (
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
 	msgraphauth "github.com/microsoftgraph/msgraph-sdk-go-core/authentication"
 	msgraphmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	providerErrors "github.com/ubuntu/authd-oidc-brokers/internal/providers/errors"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
@@ -230,20 +231,20 @@ func (p Provider) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.Password}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
+			offeredModes = []string{authmodes.Device}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.Password}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 	}
 

authd-oidc-brokers/internal/providers/noprovider/noprovider.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
 	"golang.org/x/oauth2"
 )
@@ -50,20 +51,20 @@ func (p NoProvider) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.Password}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
+			offeredModes = []string{authmodes.Device}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.Password}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 	}
 

authd-oidc-brokers/internal/testutils/provider.go
@@ -20,6 +20,7 @@ import (
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/go-jose/go-jose/v4"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/ubuntu/authd-oidc-brokers/internal/broker/authmodes"
 	"github.com/ubuntu/authd-oidc-brokers/internal/consts"
 	"github.com/ubuntu/authd-oidc-brokers/internal/providers/info"
 	"golang.org/x/oauth2"
@@ -415,20 +416,20 @@ func (p MockProviderInfoer) CurrentAuthenticationModesOffered(
 		if !tokenExists {
 			return nil, errors.New("user has no cached token")
 		}
-		offeredModes = []string{"password"}
+		offeredModes = []string{authmodes.Password}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 
 	default: // auth mode
-		if _, ok := endpoints["device_auth"]; ok && providerReachable {
-			offeredModes = []string{"device_auth"}
+		if _, ok := endpoints[authmodes.Device]; ok && providerReachable {
+			offeredModes = []string{authmodes.Device}
 		}
 		if tokenExists {
-			offeredModes = append([]string{"password"}, offeredModes...)
+			offeredModes = append([]string{authmodes.Password}, offeredModes...)
 		}
 		if currentAuthStep > 0 {
-			offeredModes = []string{"newpassword"}
+			offeredModes = []string{authmodes.NewPassword}
 		}
 	}
 